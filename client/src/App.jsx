658e0584079cf5b55635afe6df4f311c3e399a4e
mustafahincal <mustafahncal@gmail.com>
2024-04-23 20:43:30 +0300

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import 'regenerator-runtime/runtime';
+import MainPage from './pages/main';
+import MainLayout from './layouts/mainLayout';
+import { Routes, Route } from 'react-router-dom';
+import ParticleBg from './components/particle/ParticleBg';
+import ExplorePage from './pages/explore';
+function App() {
+  return (
+    <>
+      <ParticleBg />
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/explore" element={<ExplorePage />} />
+          <Route path="*" element={<div>not found</div>} />
+        </Routes>
+      </MainLayout>
+    </>
+  );
+}
+
+export default App;
