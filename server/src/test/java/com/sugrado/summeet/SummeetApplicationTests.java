b522b89aa31c41a0b0cc530c29bd0783bc3b2308
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2024-04-23 20:49:07 +0300

    feat: init server project

---
+++ server/src/test/java/com/sugrado/summeet/SummeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sugrado.summeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
